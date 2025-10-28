afac83519c8d54a9d1ff2f916db9a3ee64feb3a2
DanielG <danielgebeyehu45@gmail.com>
2025-10-27 23:05:49 -0700

    AGGRESSIVE NETLIFY REDEPLOY - Remove old comments, clean up axiosconfig

--- Client/src/axiosconfig.js
+++ Client/src/axiosconfig.js
@@ -1,15 +1,9 @@
-// import axios from 'axios'
-// const axiosInstance =axios.create({
-//     baseURL:'http://localhost:5000/api',
-//     //deployed version of evangadi server on render.com 
-//    baseURL: "https://new-evangadiforum-backend.onrender.com"
-// });
-// export default axiosInstance;
+// FORCE NETLIFY REDEPLOY - Updated Railway URL
 import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "https://forum-backend-production-0589.up.railway.app/api",
-  withCredentials: true, // Re-enabled now that we have the correct backend - FORCE REDEPLOY
+  withCredentials: true, // Re-enabled now that we have the correct backend
 });
 
 export default axiosInstance;
